b7e4369cb72eda47ebf2b99b078599d180dc7659
hassan <hassanak509@gmail.com>
2025-04-01 10:48:20 +0300

    created the Dashboard page first look

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,20 @@
+import Header from "../components/layout/Header";
+
+import Section from "../components/layout/Section";
+import Main from "../components/layout/Main";
+import SectionTitle from "../components/others/SectionTitle";
+
+const Dashboard = () => {
+  return (
+    <>
+      <Header />
+      <Main>
+        <Section>
+          <SectionTitle>Dashboard</SectionTitle>
+          <p>Dashboarddddd dataaaaaaaaaaaa</p>
+        </Section>
+      </Main>
+    </>
+  );
+};
+export default Dashboard;
