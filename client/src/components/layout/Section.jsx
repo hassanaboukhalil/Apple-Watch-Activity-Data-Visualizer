8a11476b5641566deef786af134156886cb08c20
hassan <hassanak509@gmail.com>
2025-04-01 10:47:07 +0300

    updated some components

--- client/src/components/layout/Section.jsx
+++ client/src/components/layout/Section.jsx
@@ -1,6 +1,11 @@
-const Section = ({ children, className, bgColor = "bg-tertiary" }) => {
+const Section = ({
+  children,
+  className,
+  bgColor = "bg-tertiary",
+  marginTop = "mt-8",
+}) => {
   return (
-    <section className={`container ${className} ${bgColor}`}>
+    <section className={`container ${className} ${bgColor} ${marginTop}`}>
       {children}
     </section>
   );
